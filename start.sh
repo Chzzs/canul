39c15485c0d11951387ff1a085a8061c13e4ba7d
Chazz <chzzs89@gmail.com>
2015-10-03 14:49:06 +0200

    Pre Material commit

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 echo "Starting server...";
 screen -S mongo -d -m mongod --dbpath .database;
 echo "Mongo server is started";
-screen -S node -d -m node app/index.js;
+screen -S node -d -m node app/index.js dev;
 echo "Node server is started";
